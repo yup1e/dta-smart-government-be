2286f8370692ba334e2e2bc6fc96b8df8d31edc4
yup1e <yusupmail@gmail.com>
2021-03-22 09:35:55 +0700

    base project

---
+++ src/main/java/com/telkom/finalproject/FinalProjectDtaApplication.java
@@ -0,0 +1,13 @@
+package com.telkom.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectDtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectDtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telkom/finalproject/FinalProjectDtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telkom.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectDtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
